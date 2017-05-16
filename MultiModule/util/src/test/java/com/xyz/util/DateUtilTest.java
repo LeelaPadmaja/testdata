7c06c2e0fdecbf51c55010e1f778deb468850c2f
LeelaPadmaja <leela@cloudboxonline.com>
2017-05-16 22:16:19 +0530

    Implemented Sleep so that test duration will be high

--- MultiModule/util/src/test/java/com/xyz/util/DateUtilTest.java
+++ MultiModule/util/src/test/java/com/xyz/util/DateUtilTest.java
@@ -9,11 +9,11 @@ import org.junit.Test;
 
 public class DateUtilTest {
 
-        @Test
-        public void testGetToday() {
-                String actual = DateUtil.getToday();
-                String expected = new SimpleDateFormat("dd-MMM-yyyy")
-                                .format(new Date());
-                assertEquals(expected, actual);
-        }
+    @Test
+    public void testGetToday() throws InterruptedException {
+	Thread.sleep(100);
+	String actual = DateUtil.getToday();
+	String expected = new SimpleDateFormat("dd-MMM-yyyy").format(new Date());
+	assertEquals(expected, actual);
+    }
 }
--- MultiModule/util/src/test/java/com/xyz/util/StringUtilsLengthTest.java
+++ MultiModule/util/src/test/java/com/xyz/util/StringUtilsLengthTest.java
@@ -7,22 +7,25 @@ import java.util.Date;
 
 import org.junit.Test;
 
-
 public class StringUtilsLengthTest {
 
     @Test
-    public void testLengthOfString() {
+    public void testLengthOfString() throws InterruptedException {
+	Thread.sleep(10);
 	String s = "Hello";
 	assertEquals("5", StringUtils.length(s));
     }
+
     @Test
-    public void testLengthOfEmptyString() {
+    public void testLengthOfEmptyString() throws InterruptedException {
+	Thread.sleep(30);
 	String s = "";
 	assertEquals("0", StringUtils.length(s));
     }
-    
+
     @Test
-    public void testLengthOfSpaceString(){
+    public void testLengthOfSpaceString() throws InterruptedException {
+	Thread.sleep(10000);
 	String s = " Hello World ";
 	assertEquals("13", StringUtils.length(s));
     }
